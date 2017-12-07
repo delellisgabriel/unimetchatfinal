56f091bc07721d3c1b9d919e7c7810c729df3ac8
Gabriel De Lellis <delellisgabriel@gmail.com>
2017-12-07 02:34:01 -0300

    Proyecto 2 BD UNIMETChat

---
+++ src/app/perfil/perfil.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../servicios/auth.service';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.css']
+})
+export class PerfilComponent {
+
+  nombre: string;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  actualizar() {
+    const nombre = this.nombre;
+    console.log(nombre);
+    this.authService.actualizar(nombre);
+    console.log("!!");
+  }
+
+
+}
